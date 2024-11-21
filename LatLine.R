a9e1dd5e4e99ccccd7c7ac8b33f5205c4a8fd389
fisch131 <fisch131@purdue.edu>
2024-11-21 13:24:44 -0500

    Add files via upload

--- LatLine.R
+++ LatLine.R
@@ -9,3 +9,13 @@ figA <- ggplot(data = combined) + aes(x = Activity, y = Norm.C.Width) + geom_box
 figB <- ggplot(data = combined) + aes(x = Family, y = Norm.C.Width, fill = Activity) +  geom_boxplot() +  geom_jitter() + labs(y= "Canal Width (mm/TL)") + theme_cowplot()
 fig1 <-figA + figB + plot_annotation(tag_levels = 'A')
 ggsave("LL_fig1.jpg", fig1, width = 12, height = 8)
+fig1
+figB
+
+figc <- ggplot(data = combined) + aes(x = Activity, y = Norm.C.Width, color = Family) +  geom_point() + labs(y= "Canal Width (mm/TL)") +  theme_cowplot()
+figc
+figd <- ggplot(data = combined) + aes(x = Family, y = Norm.C.Width, color = Activity) + geom_point() + labs(y= "Canal Width (mm/TL)") + theme_cowplot()
+figd
+fig2 <-figc + figd + plot_annotation(tag_levels = 'A')
+fig2
+ggsave("LL_fig2.jpg", fig2, width = 12, height = 8)
