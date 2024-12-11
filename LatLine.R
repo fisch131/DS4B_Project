7c99dd0981c2eed8b488bd5bdd761e9a58869a01
fisch131 <fisch131@purdue.edu>
2024-12-11 15:00:23 -0500

    Add files via upload
    updated code

--- LatLine.R
+++ LatLine.R
@@ -5,17 +5,66 @@ setwd("C:/Users/Melanie/Downloads")
 morph <- read_csv("LL.Morph.csv")
 activity <- read_csv("LL.Activity.csv")
 combined <- inner_join(morph, activity)
-figA <- ggplot(data = combined) + aes(x = Activity, y = Norm.C.Width) + geom_boxplot() +  geom_jitter() + labs(y= "Canal Width (mm/TL)") +  theme_cowplot()
-figB <- ggplot(data = combined) + aes(x = Family, y = Norm.C.Width, fill = Activity) +  geom_boxplot() +  geom_jitter() + labs(y= "Canal Width (mm/TL)") + theme_cowplot()
-fig1 <-figA + figB + plot_annotation(tag_levels = 'A')
-ggsave("LL_fig1.jpg", fig1, width = 12, height = 8)
-fig1
-figB
+diurnal <- filter(combined, Activity == "D") #66 species, 19 families
+nocturnal <- filter(combined, Activity == "N") #38 14 families
 
-figc <- ggplot(data = combined) + aes(x = Activity, y = Norm.C.Width, color = Family) +  geom_point() + labs(y= "Canal Width (mm/TL)") +  theme_cowplot()
-figc
-figd <- ggplot(data = combined) + aes(x = Family, y = Norm.C.Width, color = Activity) + geom_point() + labs(y= "Canal Width (mm/TL)") + theme_cowplot()
-figd
-fig2 <-figc + figd + plot_annotation(tag_levels = 'A')
+#violin plots of three parameters: figure 1
+figA <- ggplot(data = combined) + aes(x = Activity, y = Norm.PA) +  geom_violin() + geom_point() + labs(y= "Pore Area (mm2/TL)") +  theme_cowplot()
+#violin plot of canal neuromasts
+figB <- ggplot(data = combined) + aes(x = Activity, y = Norm.CN) +  geom_violin() + geom_point() + labs(y= "Canal Neuromasts (#/TL)") +  theme_cowplot()
+#violin plot of visible scale area
+figC <- ggplot(data = combined) + aes(x = Activity, y = Norm.C.Width) +  geom_violin() + geom_point() + labs(y= "Canal Width (mm/TL)") +  theme_cowplot()
+fig1 <-figA + figB + figC + plot_annotation(tag_levels = 'A')
+fig1
+
+#aim 2: figures 2-4
+fig2 <- ggplot(data = nocturnal) + aes(x = Family, y = Norm.C.Width) + geom_point(size = 2) + labs (y = "Canal Width (mm/TL)") + theme_cowplot()
 fig2
-ggsave("LL_fig2.jpg", fig2, width = 12, height = 8)
+#apogonidae and myctophidae seem to be widening canals (see literature for which species have been noted to do this), also look and see which species have been noted to use replacement neuromasts- should be ones with higher CN values- wait that should be superficial ones not canal neuromasts
+fig3 <- ggplot(data = nocturnal) + aes(x = Family, y = Norm.CN) + geom_point(size = 2) + labs (y = "Canal Neuromasts (#/TL)") + theme_cowplot()
+fig3
+#a species of serranid and sciaenid (see which ones specifically: equetus punctatus and rypictus saponaceus)
+fig4 <- ggplot(data = nocturnal) + aes(x = Family, y = Norm.PA) + geom_point(size = 2) + labs (y = "Pore Area (mm2/TL)") + theme_cowplot()
+fig4
+#same two families with higher values/wider pores
+
+#pca- figure 5
+library(ggbiplot)
+noct.pca <- prcomp (~ Norm.CN + Norm.C.Width + Norm.PA, data = nocturnal,
+                    scale = T,
+                    center = T)
+noct_plot <- ggbiplot(noct.pca, obs.scale = 1, var.scale = 1, varname.size = 6, varname.adjust = 1.6) + geom_point(aes(color = nocturnal$Family), size = 4) + theme_cowplot() + labs(color = "Family")
+print(noct_plot)
+
+#correlation- figure 6
+library(ggpubr)
+cor.test(combined$Norm.VSA, combined$Norm.CN)
+fig6 <- ggplot(data = combined) + aes(x = Norm.VSA, y = Norm.CN) +geom_point(size = 2) +geom_smooth(se = F, method = "lm", color = "red") + stat_cor(method = "pearson", label.x = 0.15, label.y = 2, size = 6)+ labs(x = "Visible Scale Area (mm2/TL)", y = "Canal Neuromasts (#/TL)") + theme_cowplot()
+fig6
+
+#t-tests comparing two groups
+library(lsr)
+t.test(diurnal$Norm.PA, nocturnal$Norm.PA) #p-value = 0.320
+cohensD(diurnal$Norm.PA, nocturnal$Norm.PA) #cohen's d = 0.251
+
+t.test(diurnal$Norm.CN, nocturnal$Norm.CN) #p-value = 0.034
+cohensD(diurnal$Norm.CN, nocturnal$Norm.CN) #cohen's d = 0.367
+
+t.test(diurnal$Norm.C.Width, nocturnal$Norm.C.Width) #p-value = 0.074
+cohensD(diurnal$Norm.C.Width, nocturnal$Norm.C.Width) #cohen's d = 0.441
+
+#other variables measured but not using
+t.test(diurnal$Norm.Scale.N, nocturnal$Norm.Scale.N) #p-value = 0.036
+t.test(diurnal$TL, nocturnal$TL) #p-value = 0.579
+
+#anovas for comparing families and post-hoc tests
+pa_aov <- aov(nocturnal$Norm.PA~factor(nocturnal$Family)) 
+summary(pa_aov) 
+pa_results <- TukeyHSD(pa_aov, conf.level = 0.95)
+pa_results
+cn_aov <- aov(nocturnal$Norm.CN~factor(nocturnal$Family)) 
+summary(cn_aov)
+cn_results <- TukeyHSD(cn_aov, conf.level = 0.95)
+cn_results
+cw_aov <- aov(nocturnal$Norm.C.Width~factor(nocturnal$Family)) 
+summary(cw_aov)
